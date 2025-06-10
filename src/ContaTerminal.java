9843dec49b46b159bcc9d1ab96f0cb7a5a2f1824
Thiago Marim <thiago.marim2005@gmail.com>
2025-06-10 00:13:54 -0300

    feat: add contaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
+        String nomeCliente;
+        String agencia;
+        int numero;
+        Double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, Digite o seu nome: ");
+        nomeCliente = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência! ");
+        agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua Conta!");
+        numero = scanner.nextInt();
+
+        System.out.println("Por fim, digite o valor a ser depositado!");
+        saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + " , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
